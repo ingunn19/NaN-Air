31b8b7c2573a5415411907d222d8eebb22be9edd
alexandersvarfdal <alexander18@ru.is>
2019-12-04 11:35:47 +0000

    second commit of the class DataAPI.

--- dataLayer.py
+++ dataLayer.py
@@ -1,15 +1,17 @@
 #data layer
 
-class Data:
+
+class DataAPI:
+
     def __init__(self, name_of_file):
         self.name_of_file = name_of_file
 
 
     def write_file(self, info):
         '''writes into an existing file , if the file does not exist the program creates it.'''
-        with open(f'csv_{self.name_of_file}_data.csv', 'a') as __csv_data:
+        with open(f'./Data/{self.name_of_file}.csv', 'a') as __csv_data:
             for line in info:
-                __csv_data.write(line + '\n')
+                __csv_data.write(','.join(line) + '\n')
 
 
     def read_file(self):
@@ -17,14 +19,22 @@ class Data:
         if the file does not extis None get sent up to the logic worker
         the logic worker then calls the write methood and a new file will be created.
         first line in the file will be names of the columns'''
-
         try:
-            with open(f'csv_{self.name_of_file}_data.csv', 'r') as __csv_data:
-                return __csv_data
+            with open(f'./Data/{self.name_of_file}.csv', 'r') as __csv_data:
+                self.__data_list = [line.strip().split(",") for line in __csv_data]
+                return self.__data_list
+
         except FileNotFoundError:
             return None
 
 
+
+
+
+jon = DataAPI('HTML')
+print(jon.read_file())
+jon.write_file([['hallo', 'halo', 'gigg', 'lagg', 'echo'], ['lol', 'lass', 'igggy', 'epsylon', 'lorem']])
+print(jon.read_file())
 
 
 
