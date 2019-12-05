eb8c28361089ac45b75a33ccfe81b3640246da41
Ingunn Káradóttir <ingunn19@ru.is>
2019-12-05 13:37:27 +0000

    sorting files

--- data_layer/dataLayer.py
+++ data_layer/dataLayer.py
@@ -1,6 +1,5 @@
 #data layer
 
-
 class DataAPI:
 
     def __init__(self, name_of_file):
@@ -9,7 +8,7 @@ class DataAPI:
 
     def write_file(self, info):
         '''writes into an existing file , if the file does not exist the program creates it.'''
-        with open(f'./Data/{self.name_of_file}.csv', 'a') as __csv_data:
+        with open(f'./csv_data/{self.name_of_file}.csv', 'a') as __csv_data:
             for line in info:
                 __csv_data.write(','.join(line) + '\n')
 
@@ -20,21 +19,12 @@ class DataAPI:
         the logic worker then calls the write methood and a new file will be created.
         first line in the file will be names of the columns'''
         try:
-            with open(f'./Data/{self.name_of_file}.csv', 'r') as __csv_data:
+            with open(f'./csv_data/{self.name_of_file}.csv', 'r') as __csv_data:
                 self.__data_list = [line.strip().split(",") for line in __csv_data]
                 return self.__data_list
 
         except FileNotFoundError:
             return None
-
-
-
-
-
-jon = DataAPI('HTML')
-print(jon.read_file())
-jon.write_file([['hallo', 'halo', 'gigg', 'lagg', 'echo'], ['lol', 'lass', 'igggy', 'epsylon', 'lorem']])
-print(jon.read_file())
 
 
 
