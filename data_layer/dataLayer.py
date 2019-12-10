e6d4fde7ba6aba131036b892a11c0825008dac67
ingunn19 <ingunn19@ru.is>
2019-12-10 16:39:01 +0000

    fixed file reading

--- data_layer/dataLayer.py
+++ data_layer/dataLayer.py
@@ -1,5 +1,3 @@
-import os
-
 class DataAPI:
 
     def __init__(self, name_of_file):
@@ -8,8 +6,7 @@ class DataAPI:
 
     def write_file(self, info):
         '''writes into an existing file , if the file does not exist the program creates it.'''
-        __file_path = os.path.realpath(f'../csv_data/{self.name_of_file}')
-        with open(__file_path, 'w') as __csv_data:
+        with open(f'.//csv_data//{self.name_of_file}.csv', 'w') as __csv_data:
             for line in info:
                 __csv_data.write(','.join(line) + '\n')
 
@@ -19,9 +16,8 @@ class DataAPI:
         if the file does not extis None get sent up to the logic worker
         the logic worker then calls the write methood and a new file will be created.
         first line in the file will be names of the columns'''
-        __file_path = os.path.realpath(f'../csv_data/{self.name_of_file}')
         try:
-            with open(__file_path, 'r') as __csv_data:
+            with open(f'.//csv_data//{self.name_of_file}.csv', 'r') as __csv_data:
                 self.__data_list = [line.strip().split(",") for line in __csv_data]
                 return self.__data_list
 
