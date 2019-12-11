3028eeacab7077dbcb4c2a65b6a644fd8945a840
alexandersvarfdal <alexander18@ru.is>
2019-12-11 15:50:43 +0000

    add_new_destination class made

---
+++ logic_layer/add_new_destination.py
@@ -0,0 +1,13 @@
+from logic_parent import LogicParent
+class AddNewDestinaion(LogicParent):
+
+    def __init__(self,__id,__destination,__travel_time,__contact_name,__contact_number):
+        self.__id = id
+        self.__destination = __destination
+        self.__travel_time = __travel_time
+        self.__contact_name = __contact_name
+        self.__contact_number = __contact_number
+
+    def add_destination(self):
+
+
