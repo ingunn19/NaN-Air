4295b5394dbc79df221a62320cd8a7e7b68e5573
Ingunn Káradóttir <ingunn19@ru.is>
2019-12-12 12:06:43 +0000

    fixing variable names

--- logic_layer/new_work_trip.py
+++ logic_layer/new_work_trip.py
@@ -8,7 +8,7 @@ class New_WorkTrip(LogicParent):
     def check_employee_availability(self, employee_id, day):
         """Takes in employee_id and time of day, checks if employee is busy that day
             returns None if busy, True if available"""
-        __flight_list = self.__flight_records.read_file()
+        __flight_list = self.flight_records.read_file()
         __employee_shifts = []
         for line in __flight_list:
             if day == (line[3][:10] or line[4][:10]):
@@ -21,7 +21,7 @@ class New_WorkTrip(LogicParent):
             has passed from it's last flight to the new departure time
             returns None if aircraft not ready, True if available"""
         plane_trips = []
-        __flight_records = self.__flight_records.read_file()
+        __flight_records = self.flight_records.read_file()
         for line in __flight_records:
             if plane_insignia in line:
                 plane_trips.append(line)
@@ -50,7 +50,7 @@ class New_WorkTrip(LogicParent):
         """Takes in a time and date, reads the flight records
             and checks if the time and date are not in use
             return None if timeslot is busy, True if timeslot is available"""
-        flight_list = self.__flight_records.read_file()
+        flight_list = self.flight_records.read_file()
         for line in flight_list:
             if time_slot in line:
                 return True
@@ -60,7 +60,7 @@ class New_WorkTrip(LogicParent):
         """Checks if destination is registered
             returns True if registered, None if not"""
         destination_to_check = destination
-        destination_list = self.__destinations.read_file()
+        destination_list = self.destinations.read_file()
         for line in destination_list:
             if destination_to_check in line:
                 return True
@@ -68,7 +68,7 @@ class New_WorkTrip(LogicParent):
 
     def get_aircraft_type(self, plane_insignia):
         """Takes in plane insignia and returns aircraft type ID"""
-        planes_list = self.__aircraft.read_file()
+        planes_list = self.aircraft.read_file()
         for line in planes_list:
             if plane_insignia in line:
                 return planes_list[planes_list.index(line)][1]
@@ -80,7 +80,7 @@ class New_WorkTrip(LogicParent):
             Saves the edited file and returns the new trip's flight ID"""
         __FLIGHT_ID = 0
         __DEPARTURE = 3
-        __flight_record_list = self.__flight_records.read_file()
+        __flight_record_list = self.flight_records.read_file()
 
         # Pop header, append new trip and sort by departure time
         __header = __flight_record_list[0]
@@ -102,13 +102,13 @@ class New_WorkTrip(LogicParent):
 
         # Return header to top and save updated records to file
         __flight_record_list.insert(0, __header)
-        self.__flight_records.write_file(__flight_record_list)
+        self.flight_records.write_file(__flight_record_list)
         return __new_id
 
     def get_travel_time(self, destination):
         """Takes in a destination, reads destination information file
             returns the travel time to destination"""
-        destination_list = self.__destinations.read_file()
+        destination_list = self.destinations.read_file()
         for line in destination_list:
             if destination in line:
                 return line[2]
